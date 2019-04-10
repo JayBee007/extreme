4d5184b44ff9af6b3162b37b53dae1cfef4d1e30
Javed <d.bekhlim@rambler-co.ru>
2019-04-10 11:59:17 +0300

    feat: add snake movement

--- canvas-exp/app.js
+++ canvas-exp/app.js
@@ -6,13 +6,25 @@ const SNAKE_BORDER_COLOR = "darkgreen";
 let snake = [
   { x: 150, y: 150 },
   { x: 140, y: 150 },
-  {x: 130, y: 150 },
-  {x: 120, y: 150 },
-  {x: 110, y: 150 }
+  { x: 130, y: 150 },
+  { x: 120, y: 150 },
+  { x: 110, y: 150 }
 ];
 
 let dx = 10;
 let dy = 0;
+
+const gameCanvas = document.getElementById("gameCanvas");
+
+const ctx = gameCanvas.getContext("2d");
+
+function clearCanvas() {
+  ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
+  ctx.strokeStyle = CANVAS_BORDER_COLOR;
+
+  ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
 
 function drawSnakePart(snakePart) {
   ctx.fillStyle = SNAKE_COLOR;
@@ -22,29 +34,58 @@ function drawSnakePart(snakePart) {
 }
 
 function advanceSnake() {
-  const head = {x: snake[0].x + dx, y: snake[0].y + dy}
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(head);
   snake.pop();
 }
 
-
 function drawSnake() {
-  snake.forEach(drawSnakePart)
+  snake.forEach(drawSnakePart);
 }
 
-const gameCanvas = document.getElementById("gameCanvas");
+function changeDirection(event) {
+  console.log('key')
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
 
-const ctx = gameCanvas.getContext("2d");
+  const keyPressed = event.keyCode;
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
 
-ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
-ctx.strokeStyle = CANVAS_BORDER_COLOR;
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
 
-ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
 
-drawSnake();
-advanceSnake()
-dx = 0;
-dy = -10;
-advanceSnake();
-drawSnake();
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+}
+
+document.addEventListener('keydown', changeDirection);
+function main() {
+  setTimeout(function() {
+    clearCanvas();
+    advanceSnake();
+    drawSnake();
+
+    main();
+  }, 100);
+}
+
+main();
