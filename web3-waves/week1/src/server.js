53c3c98b14451decc090287027ea20fb63f28cf3
Javed <d.bekhlim@rambler-co.ru>
2019-05-22 14:15:28 +0300

    chore: add PORT

--- web3-waves/week1/src/server.js
+++ web3-waves/week1/src/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname});
+  res.sendFile("public/index.html", { root: __dirname });
 });
 
-app.listen(5000, () => {
-  console.log('server running')
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server running");
+});
