13360eb7d6fe58febd3d8d9b6c199b2490d42cc0
Javed <d.bekhlim@rambler-co.ru>
2019-08-05 00:14:16 +0300

    feat: add getting and deleting task by id

--- nest-exp/src/tasks/tasks.service.ts
+++ nest-exp/src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import * as uuid from 'uuid/v1';
 
 import { Task } from '../../dist/tasks/tasks.model';
 import { TaskStatus } from './tasks.model';
+import { CreateTaskDTO } from './dto/createTask.dto';
 
 @Injectable()
 export class TasksService {
@@ -12,15 +13,24 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: CreateTaskDTO): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
       title,
       description,
       status: TaskStatus.OPEN,
-    }
+    };
 
     this.tasks.push(task);
     return task;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter(task => task.id !== id);
   }
 }
