9dfe53807283cee4560bd4e6b3f5f5935cdee2d1
Javed <d.bekhlim@rambler-co.ru>
2019-08-04 23:42:44 +0300

    feat: add task module

---
+++ nest-exp/src/tasks/tasks.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) { }
+
+  @Get('/')
+  async getAllTasks(): Promise<Task[]> {
+    return await this.taskService.getAllTasks();
+  }
+
+  @Post('/')
+  async createTask(
+    @Body('title') title: string,
+    @Body('description') description: string): Promise<Task> {
+    return await this.taskService.createTask(title, description);
+  }
+}
