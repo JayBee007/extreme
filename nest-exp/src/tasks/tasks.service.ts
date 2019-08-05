5be249cf88361a858bb96f3ee03f8550b945997c
Javed <d.bekhlim@rambler-co.ru>
2019-08-05 10:35:43 +0300

    feat: finish CRUD for tasks

--- nest-exp/src/tasks/tasks.service.ts
+++ nest-exp/src/tasks/tasks.service.ts
@@ -4,6 +4,7 @@ import * as uuid from 'uuid/v1';
 import { Task } from '../../dist/tasks/tasks.model';
 import { TaskStatus } from './tasks.model';
 import { CreateTaskDTO } from './dto/createTask.dto';
+import { GetTasksFilterDto } from './dto/getTasksFilter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,6 +12,22 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter(task => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
+    }
+
+    return tasks;
   }
 
   createTask(createTaskDto: CreateTaskDTO): Task {
@@ -32,5 +49,11 @@ export class TasksService {
 
   deleteTaskById(id: string): void {
     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
 }
