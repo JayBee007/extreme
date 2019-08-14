017c3fe174bd21fba08f5001494c403347114746
Javed <d.bekhlim@rambler-co.ru>
2019-08-14 18:32:38 +0300

    chore: connect DB

--- nest-exp1/src/app.module.ts
+++ nest-exp1/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+import { typeOrmConfg } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfg),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
