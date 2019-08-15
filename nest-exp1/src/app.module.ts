9201aff733fbdfa28edfec0d0587c95e446672a9
Javed <d.bekhlim@rambler-co.ru>
2019-08-15 10:10:48 +0300

    feat: add CRUD idea

--- nest-exp1/src/app.module.ts
+++ nest-exp1/src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-
 import { typeOrmConfg } from './config/typeorm.config';
+import { IdeaModule } from './idea/idea.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfg),
     UserModule,
+    IdeaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
