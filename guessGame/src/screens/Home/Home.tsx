74cf7da0358f03beef6712a51cb75795feff7c0d
Javed <d.bekhlim@rambler-co.ru>
2019-08-23 14:04:25 +0300

    feat: add reset btn handler

--- guessGame/src/screens/Home/Home.tsx
+++ guessGame/src/screens/Home/Home.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Button, TouchableWithoutFeedback, Keyboard, Platform } from 'react-native';
 
 import Header from '../../components/Header';
 import Container from '../../components/Container';
@@ -6,42 +8,69 @@ import Text from '../../components/Text';
 import Card from '../../components/Card';
 import TextInput from '../../components/TextInput';
 
+
+const StyledRowView = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const ButtonView = styled.View`
+  width: 100px;
+  background: ${({ background } : {background: string }) => background};
+`;
+
 const Home = () => {
   const [inputText, setInputText] = useState('');
+
   const handleInputChange = (text: string) => {
     const newText = text.replace(/[^0-9]/g, '');
-    console.log('newText', newText);
     setInputText(newText);
   };
+
+  const handleReset = () => {
+    setInputText("");
+    Keyboard.dismiss();
+  }
+
   return (
-    <React.Fragment>
-      <Header title="Guess the number" />
-      <Container padding={10}>
-        <Text
-          textAlign="center"
-          fontWeight={800}
-          fontSize={21}
-          color="#48BB78"
-          style={{ marginBottom: 10 }}
-        >
-          Start a new game!
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <Header title="Guess the number" />
+        <Container padding={10}>
+          <Text
+            textAlign="center"
+            fontWeight={800}
+            fontSize={21}
+            color="#48BB78"
+            style={{ marginBottom: 10 }}
+          >
+            Start a new game!
         </Text>
-        <Card elevation={5}>
-          <Text textAlign="center" style={{ marginBottom: 20 }}>
-            Select a number
+          <Card elevation={5}>
+            <Text textAlign="center" style={{ marginBottom: 20 }}>
+              Select a number
           </Text>
-          <TextInput
-            blurOnSubmit
-            autoCapitalize="none"
-            autoCorrect={false}
-            keyboardType="number-pad"
-            maxLength={2}
-            onChangeText={handleInputChange}
-            value={inputText}
-          />
-        </Card>
+            <TextInput
+              blurOnSubmit
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="number-pad"
+              maxLength={2}
+              onChangeText={handleInputChange}
+              value={inputText}
+            />
+            <StyledRowView>
+              <ButtonView background="#E53E3E">
+                <Button title="Reset" onPress={handleReset} color={Platform.OS === 'ios' ? '#fff' : '#E53E3E' } />
+                </ButtonView>
+              <ButtonView background="#68D391">
+                <Button title="Confirm" onPress={() => { }} color={Platform.OS === 'ios' ? '#fff' : '#68D391'} />
+                </ButtonView>
+            </StyledRowView>
+          </Card>
+        </Container>
       </Container>
-    </React.Fragment>
+    </TouchableWithoutFeedback>
   );
 };
 
